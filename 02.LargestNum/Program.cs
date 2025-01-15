598710ffef5404907843893bfc3040c4ba082b78
Ventsislav Patsev <164578745+vpatsev@users.noreply.github.com>
2025-01-15 22:56:35 +0200

    Add files via upload

---
+++ 02.LargestNum/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.Serialization;
+
+int a = int.Parse(Console.ReadLine()); 
+int b = int.Parse(Console.ReadLine()); 
+int c = int.Parse(Console.ReadLine());
+
+if (a > b)
+{
+    if (a > c)
+    {
+        Console.WriteLine(a);
+    }
+}
+ if (b > a)
+{
+    if (b > c) { Console.WriteLine(b); }
+}
+ if (c > a)
+{
+    if (c > b) { Console.WriteLine(c); };
+};
+
